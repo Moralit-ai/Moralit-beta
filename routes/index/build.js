c43b36e2194e81b8af300101ee186cd6e39c609e
FrancescoSTL <francescosapolizzi@gmail.com>
2016-04-02 21:52:01 -0500

    Show pre-processed ethical convo.

--- routes/index/build.js
+++ routes/index/build.js
@@ -2,7 +2,7 @@ module.exports = function(question, callback) {
 
 
 	var apiai = require('apiai');
-	var app = apiai("YOUR KEY", "YOUR KEY");
+	var app = apiai("76ccb7c7acea4a6884834f6687475222", "8b3e68f16ac6430cb8c40d49c315aa7a");
 
 		var request = app.textRequest(question);
 		 
@@ -14,7 +14,7 @@ module.exports = function(question, callback) {
 		    	else
 		    		callback("Unfortunately, I'm not smart enough to answer that question yet.")
 		    else if(kantSucks == 1)
-		    	callback("I'll search the web! (Still need to process using utility ethics)");
+		    	callback("I'll check with that after I pass through my utility ethic function. Heres what I would say otherwise: " + response.result.metadata.speech);
 		    else
 		    	callback(kantSucks)
 		});
